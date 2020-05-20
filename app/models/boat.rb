8ec2407cc1867932ef0b0e757a2cd496bd676f7c
rn <raulengo@gmail.com>
2020-05-20 12:27:52 +0100

    seeding part 2

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,5 @@
 class Boat < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-Boat.destroy_all
-Booking.destroy_all
 
 user = User.create(email: 'joeladamdickens@gmail.com', password: 'password', name: 'joel', last_name: 'dickens')
 
