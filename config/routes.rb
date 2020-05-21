e0bb024617ee728170507fa556d75f5e1fc9050d
zita kerepesi <kerepesi.zita@me.com>
2020-05-21 12:43:27 +0100

    edit booking is done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'boats#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :boats, only: [:new, :create, :index, :show] do
-    resources :bookings, only: [:new, :create, :update]
+    resources :bookings, only: [:new, :create, :edit, :update]
   end
   resources :bookings, only: [:show, :index]
 end
