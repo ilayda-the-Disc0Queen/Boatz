15157f92e7f1d4e5a0e0065690b519916f106235
zita kerepesi <kerepesi.zita@me.com>
2020-05-21 18:14:51 +0100

    geocode fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_143100) do
+ActiveRecord::Schema.define(version: 2020_05_21_170248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,9 @@ ActiveRecord::Schema.define(version: 2020_05_21_143100) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-<<<<<<< HEAD
+    t.text "description"
     t.float "latitude"
     t.float "longitude"
-=======
->>>>>>> 299a500c102a85663ede56da6933ec198939e535
-    t.text "description"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
