39a82edc4da6c2bf06cb7d4402f095edfa603e92
Joel Dickens <joeladickens@gmail.com>
2020-05-22 12:48:11 +0100

    implemented search bar

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,11 @@ class Boat < ApplicationRecord
   has_many :bookings, dependent: :destroy
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_location,
+    against: [ :name, :location ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
